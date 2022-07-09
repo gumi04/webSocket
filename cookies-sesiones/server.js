bffcb57ad5b07da6e0c227ed44c2b056af7c1768
Gumaro Contreras <gumaroconter123@hotmail.com>
2022-07-08 22:46:22 -0500

    antes de cookies

--- cookies-sesiones/server.js
+++ cookies-sesiones/server.js
@@ -13,7 +13,12 @@ app.set('view engine', 'ejs');
 //para pasar archivos estaticos se indica a express para configurarlo se le indica la carpeta
 // crea un middleware para el sctack del framework .use
 // se le puede dar una ruta para que el cliente los consuma y no choque con los endpoints
-app.use('/assets',express.static('assets')); //middleware
+app.use('/assets',express.static('assets',{
+    //se pone en falso el etag en el cache esto para que lo genere y se almacene en el cache
+    //max age le indica al cache cuanto tiene que durar un recurso
+    etag : false,
+    maxAge: '5h'
+})); //middleware
 
 //le indicamos a la app que use body parse para como parter de express para leer el cuerpo de una peticion en urlEncode
 app.use( bodyParser.urlencoded({extended: true}));
