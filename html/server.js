026675368b12cf270b4413c120fc4aacdd5c8fe7
Gumaro Contreras <gumaroconter123@hotmail.com>
2022-07-08 21:44:46 -0500

    se agrega primer index.html

--- html/server.js
+++ html/server.js
@@ -7,11 +7,22 @@ const app = express();
 //le indicamos a la app que use body parse para como parter de express para leer el cuerpo de una peticion en urlEncode
 app.use( bodyParser.urlencoded({extended: true}))
 
+//home
+app.get('/', (req,res)=>{
+    //se tiene que especificar mediante un json o en duro
+    // __dirname contiene el path del proyecto
+    res.sendFile('index.html',{
+        root: __dirname
+    }) 
+});
+
 
 //saludar a traves de un queryparam
 app.get('/saludar',(req,res)=>{
     res.send(`Hola ${req.query.name}`)
 });
+
+
 
 //endPoint de tipo post
 app.post('/saludar-body', (req,res)=>{
