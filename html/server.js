f648ebdf43cfbb0ac98fbec9816391aa81daada2
Gumaro Contreras <gumaroconter123@hotmail.com>
2022-07-08 22:08:35 -0500

    se modifica a motor de vista en ves de realizarlo en duro

--- html/server.js
+++ html/server.js
@@ -4,16 +4,30 @@ const bodyParser = require('body-parser');
 
 //puede espeficar las rutas y que respuesta dar
 const app = express();
+
+
+//para indicarle a nuestra app se le indica el motor de vista
+//el primero le indica que montaremos un motor de vista el segundo le indicamos cual es
+app.set('view engine', 'ejs');
+
+//para pasar archivos estaticos se indica a express para configurarlo se le indica la carpeta
+// crea un middleware para el sctack del framework .use
+// se le puede dar una ruta para que el cliente los consuma y no choque con los endpoints
+app.use('/assets',express.static('assets')); //middleware
+
 //le indicamos a la app que use body parse para como parter de express para leer el cuerpo de una peticion en urlEncode
-app.use( bodyParser.urlencoded({extended: true}))
+app.use( bodyParser.urlencoded({extended: true}));
 
 //home
 app.get('/', (req,res)=>{
+    //sin motor de vista
     //se tiene que especificar mediante un json o en duro
     // __dirname contiene el path del proyecto
-    res.sendFile('index.html',{
-        root: __dirname
-    }) 
+    // res.sendFile('index.html',{
+    //     root: __dirname
+    // })
+    res.render('index');
+    //con motor de vistas
 });
 
 
